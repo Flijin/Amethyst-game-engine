59699d5c332f10f3cc43161f05a6bf3495dae0c3
Flijin <it_genius@mail.ru>
2024-09-22 01:50:26 +0300

    Init commit

---
+++ Game Engine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Game_Engine;
+
+internal class Program
+{
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetConsoleWindow();
+
+    [DllImport("user32.dll")]
+    private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+    private const int SW_HIDE = 0b_0000;
+    private const int SW_SHOW = 0b_0101;
+
+    private static void Main()
+    {
+        ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+        using Window appWindow = new(800, 450, "TestLib");
+        appWindow.Run();
+    }
+}
---
+++ Game Engine/Window.cs
@@ -0,0 +1,20 @@
+﻿
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+
+namespace Game_Engine
+{
+    internal class Window : GameWindow
+    {
+        public Window(int wight, int height, string title) :
+            base(GameWindowSettings.Default, new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(wight, height),
+                Title = title,
+                
+            })
+        {
+
+        }
+    }
+}
